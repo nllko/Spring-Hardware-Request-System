38e9380e6ee6b34dd3f0779c0db1895c22317bdc
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-10-21 17:59:34 +0300

    init

---
+++ src/main/java/com/hrs/hardwarerequestsystem/HardwareRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hrs.hardwarerequestsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareRequestSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HardwareRequestSystemApplication.class, args);
+  }
+
+}
